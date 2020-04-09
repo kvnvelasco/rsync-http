a103795072f9aadfaaadcc01da42bc987c418381
kevin <me@kvnvelasco.com>
2020-04-09 12:59:49 +0800

    try something dumb to rate limit tx/rx

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,163 +1,30 @@
-use notify::{poll, DebouncedEvent};
-use notify::{RecommendedWatcher, RecursiveMode, Result, Watcher};
-use std::fs::{canonicalize, File};
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::channel;
-use std::thread::spawn;
-use std::time::Duration;
-
 use librsync_ffi::file::{Delta, Signature};
 use reqwest::StatusCode;
-use std::borrow::Borrow;
-use std::io::{Bytes, Read};
-use std::sync::Arc;
+use std::fs::{canonicalize, File};
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use std::sync::mpsc::{sync_channel, SyncSender};
+use std::sync::{Arc, Mutex};
+use std::task::Context;
 use std::thread;
-use tokio;
+use tokio::macros::support::{Future, Pin, Poll};
+use tokio::task;
+enum Message {
+    FileChanged(PathBuf),
+}
 
-fn watch() -> notify::Result<()> {
-    let mut args = std::env::args();
+fn each_file<P: AsRef<Path>>(start_path: P, func: SyncSender<Message>) {
+    let walker = walkdir::WalkDir::new(&start_path);
 
-    let mut target_path = PathBuf::from(&args.nth(1).unwrap_or(".".to_owned()));
-
-    let current_dir = std::env::current_dir().expect("Unable to get current dir");
-
-    if target_path.is_absolute() {
-        panic!("Absolute paths cannot be monitored.")
-    } else {
-        target_path = current_dir.join(target_path);
-    }
-
-    {
-        let absolute_target = canonicalize(&target_path).expect("Invalid path");
-        if !absolute_target.starts_with(&current_dir) {
-            panic!("You can only watch child directories");
-        }
-        target_path = absolute_target;
-    }
-
-    if !target_path.exists() {
-        panic!("Target path does not exist");
-    }
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
-
-    // Automatically select the best implementation for your platform.
-    // You can also access each implementation directly e.g. INotifyWatcher.
-    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(500))?;
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(
-        target_path.to_str().expect("cannot make cwd"),
-        RecursiveMode::Recursive,
-    )?;
-
-    // This is a simple loop, but you may want to use more complex logic here,
-    // for example to handle I/O.
-    loop {
-        match rx.recv() {
-            Ok(event) => {
-                if let DebouncedEvent::Write(ev) = &event {
-                    let path = ev
-                        .strip_prefix(&current_dir)
-                        .expect("Unable to strip prefix")
-                        .to_owned();
-                    tokio::spawn(async move { sync_file(path).await });
-                }
-            }
-            Err(e) => println!("watch error: {:?}", e),
+    for path in walker.into_iter() {
+        let loc = path.unwrap().path().to_path_buf();
+        if loc.is_file() {
+            func.send(Message::FileChanged(loc.to_path_buf()));
         }
     }
 }
 
-async fn sync_file<T: AsRef<Path>>(path: T) {
-    let signature = async {
-        let req = reqwest::get(&format!(
-            "http://localhost:8000/request-signature?file_path={}",
-            &path.as_ref().to_str().expect("Cannont create potato")
-        ))
-        .await
-        .expect("Unable to get response");
-
-        let status: StatusCode = req.status();
-
-        if status.as_str() == "404" {
-            None
-        } else {
-            let bytes = req.bytes().await.expect("Unable to get body");
-            if bytes.len() > 0 {
-                Some(Vec::<u8>::from(bytes.as_ref()))
-            } else {
-                None
-            }
-        }
-    }
-    .await;
-
-    match signature {
-        Some(signature) => {
-            println!(
-                "Patching file: {:?}",
-                &path.as_ref().to_str().expect("Unable to generate file")
-            );
-            let payload = {
-                let mut signature = Signature::from_bytes(signature);
-                let path = std::env::current_dir().unwrap().join(&path);
-                let (delta, _) = Delta::new(&mut signature, &path);
-                delta.to_bytes()
-            };
-
-            if payload.len() <= 32 {
-                println!("Files are identical, skipping");
-                return;
-            };
-
-            let client = reqwest::Client::new();
-            client
-                .post(&format!(
-                    "http://localhost:8000/patch-file?file_path={}",
-                    &path.as_ref().to_str().expect("Unable to generate file")
-                ))
-                .body(payload)
-                .send()
-                .await;
-        }
-        None => {
-            println!(
-                "Sending new file: {}",
-                &path.as_ref().to_str().expect("Unable to generate file")
-            );
-            let client = reqwest::Client::new();
-            let mut file = File::open(std::env::current_dir().unwrap().join(&path))
-                .expect("Unable to open file");
-            let mut payload = Vec::new();
-            file.read_to_end(&mut payload);
-            client
-                .post(&format!(
-                    "http://localhost:8000/new-file?file_path={}",
-                    &path.as_ref().to_str().expect("Unable to generate file")
-                ))
-                .body(payload)
-                .send()
-                .await;
-        }
-    };
-}
-
-fn each_file<P: AsRef<Path>, F: Fn(&Path)>(start_path: P, func: &F) {
-    let reader = std::fs::read_dir(&start_path).expect("Unable to read directory");
-    thread::sleep(Duration::from_millis(10));
-    for entry in reader {
-        let entry = entry.unwrap();
-        if (entry.path().is_dir()) {
-            each_file(&entry.path(), func.clone());
-        } else {
-            func(&entry.path())
-        }
-    }
-}
-
-#[tokio::main]
+#[tokio::main(max_threads = 32, core_threads = 8)]
 async fn main() {
     let mut args = std::env::args();
 
@@ -183,14 +50,130 @@ async fn main() {
         panic!("Target path does not exist");
     }
 
-    each_file(&target_path, &|path| {
-        let path = path
-            .strip_prefix(&current_dir)
-            .expect("Unable to strip prefix")
-            .to_owned();
+    let client: Arc<reqwest::Client> = Arc::new(reqwest::Client::new());
+    let (tx, rx) = sync_channel::<Message>(50);
 
-        tokio::spawn(async { sync_file(path).await });
+    let thread_tx = tx.clone();
+    let thread_target = target_path.clone();
+    thread::spawn(move || {
+        each_file(thread_target, thread_tx);
     });
-    // start watch
-    watch().expect("Unable to watch directory");
+
+    // ghetto ass concurrency limiter
+    // The problem is that tokio will consume futures and start them as quickly as possible
+    // without respecting concurrency limits. This suddenly opens thousands of tcp connections
+    // either being killed by the OS or DOS-ing the server.
+    // I need a better way to do this. Some kind of async worker pool queue
+
+    let concurrency_count = Arc::new(Mutex::new(0 as usize));
+    loop {
+        let mesg = rx.recv().expect("Unable to get next");
+        let client_ptr = client.clone();
+        if let Message::FileChanged(path) = mesg {
+            // open up the concurrency count, check if we have space otherwise wait for the current task
+            {
+                let mut count = concurrency_count
+                    .lock()
+                    .expect("Unable to get lock");
+                *count += 1;
+            }
+
+            let concurrency_pointer = concurrency_count.clone();
+            let handle = task::spawn(async move {
+                sync_file(path, client_ptr).await;
+                let mut count = concurrency_pointer
+                    .lock()
+                    .expect("unable to get inner lock");
+                *count -= 1;
+            });
+            {
+                let mut copy = 0;
+
+                {
+                    let mut count = concurrency_count
+                        .lock()
+                        .expect("Unable to get lock");
+                    copy = *count;
+                }
+
+                if copy > 128 {
+                    handle.await;
+                }
+            }
+        }
+    }
+}
+
+async fn sync_file<T: AsRef<Path>>(path: T, client: Arc<reqwest::Client>) {
+    let signature = async {
+        let req = client
+            .get(&format!(
+                "http://localhost:8000/request-signature?file_path={}",
+                &path.as_ref().to_str().expect("Cannont create potato")
+            ))
+            .send()
+            .await
+            .expect("Unable to get response");
+
+        let status: StatusCode = req.status();
+
+        if status.as_str() == "404" {
+            None
+        } else {
+            let bytes = req.bytes().await.expect("Unable to get body");
+            if bytes.len() > 0 {
+                Some(Vec::<u8>::from(bytes.as_ref()))
+            } else {
+                None
+            }
+        }
+    }
+    .await;
+
+    match signature {
+        Some(signature) => {
+            // println!(
+            //     "Patching file: {:?}",
+            //     &path.as_ref().to_str().expect("Unable to generate file")
+            // );
+            let payload = {
+                let mut signature = Signature::from_bytes(signature);
+                let path = std::env::current_dir().unwrap().join(&path);
+                let (delta, _) = Delta::new(signature, &path);
+                delta.to_bytes()
+            };
+
+            // if payload.len() <= 32 {
+            //     println!("Files are identical, skipping");
+            //     return;
+            // };
+
+            client
+                .post(&format!(
+                    "http://localhost:8000/patch-file?file_path={}",
+                    &path.as_ref().to_str().expect("Unable to generate file")
+                ))
+                .body(payload)
+                .send()
+                .await;
+        }
+        None => {
+            // println!(
+            //     "Sending new file: {}",
+            //     &path.as_ref().to_str().expect("Unable to generate file")
+            // );
+            let mut file = File::open(std::env::current_dir().unwrap().join(&path))
+                .expect("Unable to open file");
+            let mut payload = Vec::new();
+            file.read_to_end(&mut payload);
+            client
+                .post(&format!(
+                    "http://localhost:8000/new-file?file_path={}",
+                    &path.as_ref().to_str().expect("Unable to generate file")
+                ))
+                .body(payload)
+                .send()
+                .await;
+        }
+    };
 }
